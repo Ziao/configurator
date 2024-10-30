8790d3d664af0bc7772da72bb3d2e43e6b27cca2
Nick <nick@ziaomedia.com>
2024-10-31 00:00:59 +0100

    feat: github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+    base: command === "build" ? "/configurator/" : "/",
     build: {
         outDir: "docs",
     },
@@ -25,4 +26,4 @@ export default defineConfig({
     test: {
         environment: "happy-dom",
     },
-});
+}));
